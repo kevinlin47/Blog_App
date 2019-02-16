ecb5e2cc9f06ed39d7c1a62bd59fbad978030ea9
Kevin Lin <kevinlin47@hotmail.com>
2019-02-16 15:36:48 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,16 +4,21 @@ var bodyParser=require("body-parser");
 var mongoose=require("mongoose");
 
 mongoose.connect("mongodb://localhost:27017/restful_blog_app",{useNewUrlParser:true});
-app.set("view enginer","ejs");
+app.set("view engine","ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
+//Mongoose/Model Config
 var blogSchema=new mongoose.Schema({
 	title: String,
 	image: String,
 	body: String,
 	created: {type: Date, default: Date.now}
 });
+
+var Blog=mongoose.model("Blog",blogSchema);
+
+//RESTful routes
 
 app.listen(3000,"127.0.0.1", function(){
 	console.log("Server is running");
