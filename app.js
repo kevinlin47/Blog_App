082deb3437ad894d06dd2597bc3132d56f061f56
Kevin Lin <kevinlin47@hotmail.com>
2019-02-16 15:29:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ var express=require("express");
 var app=express();
 var bodyParser=require("body-parser");
 var mongoose=require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/restful_blog_app",{useNewUrlParser:true});
