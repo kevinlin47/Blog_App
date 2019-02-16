ca952932fefb4889317a581acef6c7212a9c5124
Kevin Lin <kevinlin47@hotmail.com>
2019-02-16 15:32:30 -0500

    Added ejs npm install
    Listening on port 3000 local host

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ var bodyParser=require("body-parser");
 var mongoose=require("mongoose");
 
 mongoose.connect("mongodb://localhost:27017/restful_blog_app",{useNewUrlParser:true});
+app.set("view enginer","ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.listen(3000,"127.0.0.1", function(){
+	console.log("Server is running");
+});
